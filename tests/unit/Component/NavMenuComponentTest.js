d802c76dabff663cfe5774bbd5d03e905f8ebb42
Daniel Phillimore <daniel.phillimore@jadu.co.uk>
2015-02-22 21:21:08 +0000

    Skeletonified

--- tests/unit/Component/NavMenuComponentTest.js
+++ tests/unit/Component/NavMenuComponentTest.js
@@ -62,98 +62,17 @@ describe('NavMenuComponent', function () {
         });
 
         describe('before any menu items are clicked', function () {
-            it('should have no visible submenus', function () {
-                expect(this.$menu.find('.expanded-nav-menu.expanded-nav-menu--visible')).to.have.length(0);
-            });
+            it('should have no visible submenus');
         });
 
         describe('after clicking the first menu item', function () {
-            it('should show the first menu item\'s submenu', function () {
-                this.$menuItem1.click();
+            it('should show the first menu item\'s submenu');
 
-                expect(this.$submenu1.hasClass('expanded-nav-menu--visible')).to.be.true;
-            });
+            it('should not show the second menu item\'s submenu');
 
-            it('should not show the second menu item\'s submenu', function () {
-                this.$menuItem1.click();
+            it('should stop propagation of the click event');
 
-                expect(this.$submenu2.hasClass('expanded-nav-menu--visible')).to.be.false;
-            });
-
-            it('should stop propagation of the click event', function () {
-                var event = $.Event('click');
-
-                this.$menuItem1.trigger(event);
-
-                expect(event.isPropagationStopped()).to.be.true;
-            });
-
-            it('should prevent the default behaviour of the click event', function () {
-                var event = $.Event('click');
-
-                this.$menuItem1.trigger(event);
-
-                expect(event.isDefaultPrevented()).to.be.true;
-            });
-        });
-
-        describe('after clicking the first and then second menu item', function () {
-            it('should hide the first menu item\'s submenu', function () {
-                this.$menuItem1.click();
-                this.$menuItem2.click();
-
-                expect(this.$submenu1.hasClass('expanded-nav-menu--visible')).to.be.false;
-            });
-
-            it('should show the second menu item\'s submenu', function () {
-                this.$menuItem1.click();
-                this.$menuItem2.click();
-
-                expect(this.$submenu2.hasClass('expanded-nav-menu--visible')).to.be.true;
-            });
-        });
-
-        describe('after clicking the first menu item then opening a sub-submenu', function () {
-            it('should show the sub submenu', function () {
-                this.$menuItem1.click();
-                this.$menuItem1_1.click();
-
-                expect(this.$submenu1_1.hasClass('expanded-nav-menu--visible')).to.be.true;
-            });
-
-            it('should not hide the sub submenu\'s parent menu', function () {
-                this.$menuItem1.click();
-                this.$menuItem1_1.click();
-
-                expect(this.$submenu1.hasClass('expanded-nav-menu--visible')).to.be.true;
-            });
-        });
-
-        describe('after clicking the first menu item then opening a sub-submenu then clicking outside the menu', function () {
-            it('should hide the sub submenu', function () {
-                this.$menuItem1.click();
-                this.$menuItem1_1.click();
-                this.$aParagraph.click();
-
-                expect(this.$submenu1_1.hasClass('expanded-nav-menu--visible')).to.be.false;
-            });
-
-            it('should hide the sub submenu\'s parent menu', function () {
-                this.$menuItem1.click();
-                this.$menuItem1_1.click();
-                this.$aParagraph.click();
-
-                expect(this.$submenu1.hasClass('expanded-nav-menu--visible')).to.be.false;
-            });
-        });
-
-        describe('after clicking the first menu item then clicking outside the menu', function () {
-            it('should hide the first menu item\'s submenu', function () {
-                this.$menuItem1.click();
-                this.$aParagraph.click();
-
-                expect(this.$submenu1.hasClass('expanded-nav-menu--visible')).to.be.false;
-            });
+            it('should prevent the default behaviour of the click event');
         });
     });
 });
