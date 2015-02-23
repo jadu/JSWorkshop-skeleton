d46fc0c14ae0eca5c372dcbaea994e3b509d10d7
Daniel Phillimore <daniel.phillimore@jadu.co.uk>
2015-02-23 08:43:53 +0000

    Tidied up

--- src/Component/NavMenuComponent.js
+++ src/Component/NavMenuComponent.js
@@ -5,16 +5,3 @@
  */
 
 'use strict';
-
-var $ = require('jquery');
-
-function NavMenuComponent($body, $menu) {
-    this.$body = $body;
-    this.$menu = $menu;
-}
-
-NavMenuComponent.prototype.initialize = function () {
-
-};
-
-module.exports = NavMenuComponent;
